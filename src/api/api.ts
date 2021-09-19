b0a59e2e496c663450369fdcc1d4a91eb228bd4b
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-09-19 23:23:55 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,9 @@
 import {getAuth} from "./auth";
 import {ModelRegistry} from "./model";
 
-export const baseUrl = "https://needmedi.com"
+// export const baseUrl = "https://needmedi.com"
 // export const baseUrl = "http://127.0.0.1:8000" 
-// export const baseUrl = "http://10.147.19.10:8080"
+export const baseUrl = "http://10.147.19.10:8080"
 
 
 export async function get(url: string, kwargs = {}, headers = {}) {
