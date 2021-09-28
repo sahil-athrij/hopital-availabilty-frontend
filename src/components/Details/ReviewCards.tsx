f00d196444fc7aec881ec93c86d1b33e1afcf3be
SANU MUHAMMED C <sanumuhammed.c@gmail.com>
2021-09-28 17:39:21 +0000

    added

---
+++ src/components/Details/ReviewCards.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {ReviewObject} from '../../api/model/model.tsx'
+
+interface ReviewObject{
+    model: ReviewObject;
+}
+
+export default class ReviewCards extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
