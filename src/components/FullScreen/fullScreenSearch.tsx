aca049d83c2e4261f1fa3de55e0bd8236acad48e
Nidhin P Pramod <kichum386@gamil.com>
2021-11-09 21:22:58 +0530

    screenfilter design corrected

--- src/components/FullScreen/fullScreenSearch.tsx
+++ src/components/FullScreen/fullScreenSearch.tsx
@@ -1,22 +1,22 @@
-import { ResponsiveComponent, ResponsiveState } from "../ResponsiveComponent";
-import { Container } from "react-bootstrap";
+import {ResponsiveComponent, ResponsiveState} from "../ResponsiveComponent";
+import {Container} from "react-bootstrap";
 import {
     FullScreenLocationProps,
     LocationSearchBoxLoc,
     LocationSearchProps,
     LocationSearchState
 } from "./FullScreenLocation";
-import { Marker } from "../../api/model";
+import {Marker} from "../../api/model";
 import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import CloseIcon from "@mui/icons-material/Close";
-import { getParam, setParam } from "../../api/QueryCreator";
-import { withRouter } from "react-router";
+import {getParam, setParam} from "../../api/QueryCreator";
+import {withRouter} from "react-router";
 import React from "react";
 import "./fullScreenSearch.css";
-import { toast } from "react-toastify";
-import { Avatar, Button, Chip, IconButton } from "@mui/material";
-import { withStyles } from "@mui/styles";
+import {toast} from "react-toastify";
+import {Avatar, Button, Chip, IconButton} from "@mui/material";
+import {withStyles} from "@mui/styles";
 import MenuIcon from "@mui/icons-material/Menu";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
@@ -24,8 +24,8 @@ import MyLocationOutlinedIcon from "@mui/icons-material/MyLocationOutlined";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 
-
-interface LocationQuerySearchProps extends LocationSearchProps {
+interface LocationQuerySearchProps extends LocationSearchProps
+{
     close: () => void,
     closeWindow?: () => void
 }
@@ -42,7 +42,8 @@ type SuggestionSearch = {
     }
 }
 
-interface LocationQuerySearchState extends LocationSearchState {
+interface LocationQuerySearchState extends LocationSearchState
+{
     suggestionsSearch: SuggestionSearch[],
     selectedSearch: number,
     value: string,
@@ -96,7 +97,7 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
 {
 
 
-    constructor(props: LocationQuerySearchProps) 
+    constructor(props: LocationQuerySearchProps)
     {
         super(props);
         this.state = {
@@ -111,19 +112,18 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
 
     }
 
-    toggleDrawer = (newOpen: boolean) => () => 
+    toggleDrawer = (newOpen: boolean) => () =>
     {
-        this.setState({ filter_active: newOpen });
+        this.setState({filter_active: newOpen});
     };
 
-    toggleLocation = (open: boolean) => () => 
+    toggleLocation = (open: boolean) => () =>
     {
-        this.setState({ location_active: open });
+        this.setState({location_active: open});
     };
 
 
-
-    setPersistence() 
+    setPersistence()
     {
         setParam("loc", this.state.value, "Select Location");
         setParam("query", this.state.query, "Search Hospital");
@@ -133,15 +133,15 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
     }
 
 
-    async SuggestLocationsSearch(event: React.ChangeEvent<HTMLInputElement>) 
+    async SuggestLocationsSearch(event: React.ChangeEvent<HTMLInputElement>)
     {
-        this.setState({ query: event.target.value }, this.setPersistence);
-        try 
+        this.setState({query: event.target.value}, this.setPersistence);
+        try
         {
-            const values = await Marker.filter({ search: this.state.query, limit: 5 });
-            this.setState({ suggestionsSearch: values.results });
+            const values = await Marker.filter({search: this.state.query, limit: 5});
+            this.setState({suggestionsSearch: values.results});
         }
-        catch (e) 
+        catch (e)
         {
             toast.error("Internet Not Available", {
                 position: "bottom-center",
@@ -150,32 +150,33 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
 
     }
 
-    handleChipChange(value: string) 
+    handleChipChange(value: string)
     {
         console.log(value);
         const index = this.state.filters.indexOf(value);
-        const { filters } = this.state;
+        const {filters} = this.state;
         if (index > -1)
-
+        
             filters.splice(index, 1);
-
+        
         else
-
+        
             filters.push(value);
+        
 
 
-        this.setState({ filters });
+        this.setState({filters});
     }
 
 
-    handleEnterSearch() 
+    handleEnterSearch()
     {
         const newSelected = this.state.suggestionsSearch[this.state.selectedSearch];
         const newValue = newSelected ? newSelected.name : this.state.query;
         this.setState({
             query: newValue,
             display: 0
-        }, () => 
+        }, () =>
         {
             this.searchCallBack();
         });
@@ -183,53 +184,53 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
     }
 
 
-    handleKeyDownSearch = (e: React.KeyboardEvent<HTMLInputElement>) => 
+    handleKeyDownSearch = (e: React.KeyboardEvent<HTMLInputElement>) =>
     {
 
-        const { selectedSearch, suggestionsSearch } = this.state;
+        const {selectedSearch, suggestionsSearch} = this.state;
         console.log(e.key);
-        if (e.key === "ArrowUp" && suggestionsSearch.length > 0) 
+        if (e.key === "ArrowUp" && suggestionsSearch.length > 0)
         {
             e.preventDefault();
             this.setState(prevState => ({
                 selectedSearch: prevState.selectedSearch === -1 ? -1 : prevState.selectedSearch - 1
             }));
         }
-        else if (e.key === "ArrowDown" && selectedSearch < suggestionsSearch.length - 1) 
+        else if (e.key === "ArrowDown" && selectedSearch < suggestionsSearch.length - 1)
         {
             e.preventDefault();
             this.setState(prevState => ({
                 selectedSearch: prevState.selectedSearch + 1
             }));
         }
-        else if (e.key === "Enter") 
+        else if (e.key === "Enter")
         {
             e.preventDefault();
             this.handleEnterSearch();
         }
     };
 
-    displaySuggestionsSearch(list: SuggestionSearch[]) 
+    displaySuggestionsSearch(list: SuggestionSearch[])
     {
-        return list.map((item: SuggestionSearch, i: number) => 
+        return list.map((item: SuggestionSearch, i: number) =>
         {
             return (
                 <Container
                     className={"w-100  py-3  select-locations " + ((i === this.state.selectedSearch) ? "active" : "")}
                     key={i}
-                    onClick={() => 
+                    onClick={() =>
                     {
                         const newValue = item.name;
                         this.setState({
                             query: newValue,
                             display: 0
-                        }, () => 
+                        }, () =>
                         {
                             this.searchCallBack();
                         });
                     }}>
 
-                    <LocalHospitalIcon sx={{ width: "30px" }} className="input-marker mr-3" />
+                    <LocalHospitalIcon sx={{width: "30px"}} className="input-marker mr-3"/>
                     <div className="fill-rest">{item.name}
                     </div>
                 </Container>
@@ -239,9 +240,7 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
     }
 
 
-
-
-    render() 
+    render()
     {
         return (
             <div>
@@ -249,7 +248,7 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
                     className={"input-holder overflow-x-hidden mb-3 mx-2 "}>
                     <div className=" w-100 d-flex justify-content-between align-self-center">
                         <IconButton>
-                            <MenuIcon />
+                            <MenuIcon/>
                         </IconButton>
 
                         <input placeholder="Search Hospital" className="main-input w-75 mx-2 align-content-start pt-1"
@@ -260,24 +259,25 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
                             {
                                 this.handleKeyDownSearch(event);
                             }}
-                            onChange={(event) => 
+                            onChange={(event) =>
                             {
                                 this.SuggestLocationsSearch(event,).then();
                             }}
-                            onFocusCapture={() => 
+                            onFocusCapture={() =>
                             {
-                                this.setState({ display: 1 });
+                                this.setState({display: 1});
                             }}
                         />
-                        {this.state.query === "" ? <Avatar className="align-self-center" sx={{ width: "28px", height: "28px" }} /> :
-                            <CloseIcon sx={{ width: 30 }} className="align-self-center" onClick={() => 
+                        {this.state.query === "" ?
+                            <Avatar className="align-self-center" sx={{width: "28px", height: "28px"}}/> :
+                            <CloseIcon sx={{width: 30}} className="align-self-center" onClick={() =>
                             {
-                                this.setState({ query: "" },
-                                    () => 
+                                this.setState({query: ""},
+                                    () =>
                                     {
                                         this.setPersistence();
                                     });
-                            }} />
+                            }}/>
                         }
 
                     </div>
@@ -292,21 +292,24 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
                     {/*        });*/}
                     {/*}}/>}*/}
                 </Container>
-                <div className="d-flex align-items-center p-2" style={{ boxShadow: "0px 6px 6.25px rgba(0, 0, 0, 0.25)" }}>
-                    <LocationOnIcon sx={{ marginRight: "auto" }} onClick={() => (this.setState({ location_active: !this.state.location_active })
-                    )} />
-                    <div className="bottombox w-100 py-1" style={{ overflowX: "auto", whiteSpace: "nowrap" }}>
+                <div className="d-flex align-items-center p-2"
+                    style={{boxShadow: "0px 6px 6.25px rgba(0, 0, 0, 0.25)"}}>
+                    <LocationOnIcon sx={{marginRight: "auto"}}
+                        onClick={() => (this.setState({location_active: !this.state.location_active})
+                        )}/>
+                    <div className="bottombox w-100 py-1" style={{overflowX: "auto", whiteSpace: "nowrap"}}>
 
                         {this.state.filters.map((value, index) => (
                             <StyledChip className="col-xs-4 mx-1" key={index} sx={{
                                 background: " #3E64FF", borderRadius: "5px", color: "white",
                                 fontSize: "8px", width: "76px", height: "21px"
-                            }} label={value} />
+                            }} label={value}/>
                         ))}
 
                     </div>
-                    <Button sx={{ textTransform: "none", marginLeft: "auto" }} endIcon={<KeyboardArrowDownIcon />} onClick={() => (this.setState({ filter_active: !this.state.filter_active })
-                    )}>
+                    <Button sx={{textTransform: "none", marginLeft: "auto"}} endIcon={<KeyboardArrowDownIcon/>}
+                        onClick={() => (this.setState({filter_active: !this.state.filter_active})
+                        )}>
                         Filter
                     </Button>
                 </div>
@@ -322,65 +325,66 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
                     disableSwipeToOpen={false}
                     ModalProps={{
                         keepMounted: true,
-                    }} className="fixed-bottom w-100 h-50 p-3" sx={{ overflowY: "auto" }}>
+                    }} className="fixed-bottom w-100 h-50 p-3" sx={{overflowY: "auto"}}>
 
                     <div className="filtertop d-flex w-100 justify-content-between pt-3 pb-2 px-3 align-self-center">
                         Search By Location
-                        <IconButton onClick={() => 
+                        <IconButton onClick={() =>
                         {
-                            this.setState({ location_active: !this.state.location_active }
+                            this.setState({location_active: !this.state.location_active}
                             );
                         }}>
-                            <CloseIcon sx={{ color: "#0338B9" }} />
+                            <CloseIcon sx={{color: "#0338B9"}}/>
                         </IconButton>
                     </div>
                     <div className="filterbottom d-flex flex-column pt-4">
 
-                        <Container className={"w-100 input-holder " + ((2 === this.state.display) ? "active-blue" : "")}>
+                        <Container
+                            className={"w-100 input-holder " + ((2 === this.state.display) ? "active-blue" : "")}>
                             {/* <MarkerSvg className=" input-marker"/> */}
 
                             <input placeholder="Select Location"
                                 className={"main-input "}
                                 type="search"
                                 value={this.state.value}
-                                onKeyDown={(event) => 
+                                onKeyDown={(event) =>
                                 {
                                     this.handleKeyDown(event);
                                 }}
-                                onFocusCapture={() => 
+                                onFocusCapture={() =>
                                 {
-                                    this.setState({ display: 2 });
+                                    this.setState({display: 2});
                                 }}
-                                onChange={(event) => 
+                                onChange={(event) =>
                                 {
                                     this.SuggestLocations(event).then();
-                                }} />
+                                }}/>
 
                             {this.state.value &&
-                                <CloseOutlinedIcon onClick={() => 
-                                {
-                                    this.setState({ value: "" },
-                                        () => 
-                                        {
-                                            this.setPersistence();
-                                        }
-                                    );
-                                }} />}
+                            <CloseOutlinedIcon onClick={() =>
+                            {
+                                this.setState({value: ""},
+                                    () =>
+                                    {
+                                        this.setPersistence();
+                                    }
+                                );
+                            }}/>}
                         </Container>
                         {(this.state.display === 2 || this.state.display === 0) &&
-                            <Container className="w-100 text-primary mt-1 select-locations py-3 pointers"
-                                onClick={() => 
-                                {
-                                    this.getLocation().then();
-                                }}>
-                                <MyLocationOutlinedIcon className="input-marker mr-3" />
-                                <div className="fill-rest">Use Current Location / Please enable Location services</div>
-                            </Container>}
+                        <Container className="w-100 text-primary mt-1 select-locations py-3 pointers"
+                            onClick={() =>
+                            {
+                                this.getLocation().then();
+                            }}>
+                            <MyLocationOutlinedIcon className="input-marker mr-3"/>
+                            <div className="fill-rest">Use Current Location / Please enable Location services</div>
+                        </Container>}
                         {this.state.display === 2 ? this.displaySuggestions(this.state.suggestions) : ""}
 
                     </div>
 
-                </SwipeableDrawer> 
+                </SwipeableDrawer>
 
                 <SwipeableDrawer anchor="bottom"
                     open={this.state.filter_active}
@@ -389,24 +393,24 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
                     disableSwipeToOpen={false}
                     ModalProps={{
                         keepMounted: true,
-                    }} className="fixed-bottom w-100 h-50 p-3" sx={{ overflowY: "auto" }}>
+                    }} className="fixed-bottom w-100 h-50 p-3" sx={{overflowY: "auto"}}>
 
                     <div className="filtertop w-100 d-flex justify-content-between pt-3 pb-2 px-3 align-self-center">
                         Select all that apply
-                        <IconButton onClick={() => 
+                        <IconButton  onClick={() =>
                         {
-                            this.setState({ filter_active: false }
+                            this.setState({filter_active: false}
                             );
                         }}>
-                            <CloseIcon sx={{ color: "#0338B9" }} />
+                            <CloseIcon sx={{color: "#0338B9"}}/>
                         </IconButton>
                     </div>
-                    <IconButton onClick={() => 
+                    <Button className="bg-grey d-flex justify-content-end" endIcon={<CloseIcon sx={{color: "#0338B9"}}/>} onClick={() =>
                     {
-                        this.setState({ filters: [] });
+                        this.setState({filters: []});
                     }}>
-                        <CloseIcon sx={{ color: "#0338B9" }} />
-                    </IconButton>
+                        clear filter
+                    </Button>
                     <div className="filterbottom d-flex flex-column">
                         <div className="filterhead w-100 mb-2 mt-4 ">Types</div>
                         <div className="chips d-flex flex-wrap ">
@@ -451,20 +455,21 @@ export class LocationQuerySearchBoxLoc extends LocationSearchBoxLoc<LocationQuer
                     </div>
 
                 </SwipeableDrawer>
- 
+
             </div>
 
         );
-        
+
     }
 
 }
 
 export const LocationQuerySearchBox = withRouter(LocationQuerySearchBoxLoc);
 
-export class FullScreenSearch extends ResponsiveComponent<FullScreenLocationProps, ResponsiveState>
+export class FullScreenSearch extends
+    ResponsiveComponent<FullScreenLocationProps, ResponsiveState>
 {
-    render() 
+    render()
     {
         return (<div className="fixed-top w-100 h-100 bg-white header">
 
