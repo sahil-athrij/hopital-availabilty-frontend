0a7db263ea12b133b3aeb3508ff53a55ff5bdfdd
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-06-29 18:55:15 +0530

    feat: added calender with conditional rendering

--- src/components/DoctorDashBoard/SlotArrange.tsx
+++ src/components/DoctorDashBoard/SlotArrange.tsx
@@ -4,8 +4,10 @@ import Button from "@mui/material/Button";
 import { Container } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { StickyHead } from "../Utils";
-import { withRouter, RouteComponentProps  } from "react-router";
+import { withRouter, RouteComponentProps } from "react-router";
 import CheckDay from "./CheckDay";
+import CustomDatePicker from "../Doctor/CustomDatePicker";
+
 
 
 
@@ -13,47 +15,48 @@ const DAYS: string[] = ["sunday", "monday", "tuesday", "wednesday", "thursday", 
 
 
 
-interface State{
+interface State {
   show: boolean;
-  clickIndex:number;
+  clickIndex: number;
   checked: boolean;
+  openCalender: boolean;
 }
 type AppProps = RouteComponentProps;
- class SlotArrange extends Component<AppProps, State>
+class SlotArrange extends Component<AppProps, State>
 {
 
-    state:State = {
-      show:false,
-      clickIndex:0,
-      checked:false
-    };
+  state: State = {
+    show: false,
+    clickIndex: 0,
+    checked: false,
+    openCalender: false,
+  };
 
-  setClickIndex(el:number)
-  {
-      this.setState({clickIndex:el});
-      console.log(this.state.clickIndex);
+  setClickIndex(el: number)
+   {
+    this.setState({ clickIndex: el });
   }
 
 
-  handleChecked = (event: React.ChangeEvent<HTMLInputElement>):void=>
-    {
-    this.setState({checked:event.target.checked});
-   };
+  handleChecked = (event: React.ChangeEvent<HTMLInputElement>): void =>
+   {
+    this.setState({ checked: event.target.checked });
+  };
 
   render() 
-    {
+  { 
     return (
       <>
-        <StickyHead title="set Weekly Schedule" onClick={() => console.log("submit")} goBack={() =>this.props.history.goBack()} />
+        <StickyHead title="set Weekly Schedule" onClick={() => console.log("submit")} goBack={() => this.props.history.goBack()} />
         <Container className="slot_body">
-          {DAYS.map((el, index)=>
-          {
+          {DAYS.map((el, index) =>
+           {
             return (
-                <CheckDay key={index} index={index} el={el} setClickIndex={()=>this.setClickIndex(index)} clickIndex={this.state.clickIndex}/>
-             );
+              <CheckDay key={index} index={index} el={el} setClickIndex={() => this.setClickIndex(index)} clickIndex={this.state.clickIndex} />
+            );
           })};
           <div className="btn-add">
-            <Button variant="outlined" size="medium">
+            <Button variant="outlined" size="medium" onClick={() => this.setState({ openCalender: !this.state.openCalender })}>
               <div className="btn-cont">
                 <span className="btn-span">
                   <AddIcon />
@@ -62,6 +65,11 @@ type AppProps = RouteComponentProps;
               </div>
             </Button>
           </div>
+          {this.state.openCalender && <>
+            <div className="cal">
+            <CustomDatePicker ranges={[{start: new Date(), end: new Date()}]} onChange={()=>console.log("changed")}/>
+            </div>
+          </>}
         </Container>
       </>
 
