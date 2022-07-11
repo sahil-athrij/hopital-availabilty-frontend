ae186398944526f523af55db88390068a5ca6477
Vaishnav <vaishnavvaichu123@gmail.com>
2022-07-11 23:05:39 +0530

    financial fix

--- src/components/Form/MUIControl.tsx
+++ src/components/Form/MUIControl.tsx
@@ -1,11 +1,12 @@
-import { Controller, UseControllerProps, Control } from "react-hook-form";
+import { Controller, UseControllerProps, Control, ControllerRenderProps } from "react-hook-form";
 
 export default function MUIController(
-    { children, control,name }: UseControllerProps & { children: (p:{ error?: any, helperText?: any })=>JSX.Element, control: Control, name:string }
-) {
+    { children, control, name }: UseControllerProps & { children: (p:{ error?: any, helperText?: any, } & ControllerRenderProps<any, string>)=>JSX.Element, control: Control, name:string }
+) 
+{
     return <Controller
         name={name}
         control={control}
         render={({ field, fieldState }) => children({ error: fieldState.error !== undefined, helperText: fieldState.error?.message, ...field })??<></>}
-    ></Controller>
+    ></Controller>;
 }
