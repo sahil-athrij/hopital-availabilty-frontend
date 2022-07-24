fa7a39614cc4773c86a8a8a069b91fc595a4e19c
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-07-24 19:04:46 +0530

    feat: added dynamic routes to view help

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,49 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
-    extends: [
-        "plugin:eslint-plugin/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        "plugin:jsx-a11y/recommended",
-        "plugin:eslint-comments/recommended",
+  },
+  extends: [
+    "plugin:eslint-plugin/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:eslint-comments/recommended",
+  ],
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/no-var-requires": "off",
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "linebreak-style": "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["@mui/*/*/*", "!@mui/material/test-utils/*"],
+      },
     ],
-    rules: {
-        "react-hooks/exhaustive-deps": "off",
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": ["error"],
-        "@typescript-eslint/no-var-requires": "off",
-        "react/prop-types": "off",
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "linebreak-style":"off",
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-restricted-imports": [
-            "error",
-            {
-                "patterns": ["@mui/*/*/*", "!@mui/material/test-utils/*"]
-            }
-        ],
-        "curly": ["warn", "multi"],
-        "comma-spacing": "warn",
-        "eol-last": "warn",
-        "eqeqeq": "error",
-        "prefer-const": "error",
-        "brace-style": ["error", "allman"],
-        "indent": ["error", 4]
-    },
+    curly: ["warn", "multi"],
+    "comma-spacing": "warn",
+    "eol-last": "warn",
+    eqeqeq: "error",
+    "prefer-const": "error",
+    // "brace-style": ["error", "allman"],
+    // "indent": ["error", 4]
+  },
 };
